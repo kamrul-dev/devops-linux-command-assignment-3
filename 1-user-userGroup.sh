ad0e7985f490ce5fb3639805c35edbc76b681e48
Kamrul-dev <kamrulsdev@gmail.com>
2023-10-06 18:06:58 +0600

    Create an user and user group by using linux command

---
+++ 1-user-userGroup.sh
@@ -0,0 +1,7 @@
+========== Create an user and user group===========
+
+1. Create a user named user1: sudo useradd user1
+
+2. Check user1 is created or not with the following command: cat /etc/passwd
+
+3. Now create an user group: sudo useradd user2 -g [user id]
