b75a12aeb4fb28026eb7e64b25f25d17e98093d5
Kamrul-dev <kamrulsdev@gmail.com>
2023-10-08 12:20:06 +0600

    Update created two directory named os-concepts and ubuntu-is-the-best

--- 2-create-directory.sh
+++ 2-create-directory.sh
@@ -1,3 +1,1 @@
-=======Create two directories in user's home directory named os-concepts and ubuntu-is-the-best=======
-
-1. Command is: mkdir os-concepts ubuntu-is-the-best
+mkdir os-concepts ubuntu-is-the-best
